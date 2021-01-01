ce9430eade3d6dc654b70f0e7acf03caa7494b47
Charles Li <charles9000li@gmail.com>
2020-12-31 19:43:53 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="polyiv",
+    version="0.1",
+    author="Charles Li",
+    author_email="charlesli@ucsb.edu",
+    url="https://github.com/charles9li/polyiv"
+)
