30b3e9ad74e2d5fc7d070d50836f3f0cfaae92ab
Charles Li <charles9000li@gmail.com>
2021-01-03 19:02:57 -0800

    added pairs data structure

--- polyiv/utils.py
+++ polyiv/utils.py
@@ -1,4 +1,24 @@
 from collections import defaultdict
+import warnings
+
+__all__ = ['Pairs', 'PairKeyDict']
+
+
+class Pairs(object):
+
+    def __init__(self):
+        self._dict = defaultdict(list)
+
+    def add_pair(self, item1, item2):
+        if (item1 in self._dict[item2]) or (item1 in self._dict[item2]):
+            warnings.warn("({}, {}) pair already exists".format(str(item1), str(item2)))
+        else:
+            self._dict[item1].append(item2)
+
+    def __iter__(self):
+        for key, item in self._dict.items():
+            for element in item:
+                yield key, element
 
 
 class PairKeyDict(object):
